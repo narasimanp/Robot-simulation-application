54f4dbd12235dbe4226129a206a8a5ca5e43a989
Narasiman <Narasiman@DESKTOP-FJQ7RNC>
2024-03-21 09:17:12 +0530

    Add project files.

---
+++ Robot simulation application/LeftCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot_simulation_application
+{
+    public class LeftCommand : ICommand
+    {
+        private readonly IRobot _robot;
+
+        public LeftCommand(IRobot robot)
+        {
+            _robot = robot;
+        }
+
+        public void Execute()
+        {
+            _robot.TurnLeft();
+        }
+    }
+
+}
---
+++ Robot simulation application/Program.cs
@@ -0,0 +1,34 @@
+﻿using Robot_simulation_application;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICommand[] commands =
+        {
+
+          new PlaceCommand(new ToyRobot(), 0, 0, Direction.NORTH), 
+            new MoveCommand(new ToyRobot()),
+            new ReportCommand(new ToyRobot()) // Output: 0,1,NORTH
+        };
+
+        ICommand[] commands2 =
+        {
+            new PlaceCommand(new ToyRobot(), 0, 0, Direction.NORTH),
+            new LeftCommand(new ToyRobot()),
+            new ReportCommand(new ToyRobot()) // Output: 0,0,WEST
+        };
+
+        CommandProcessor processor = new CommandProcessor(commands);
+        processor.ProcessCommands();
+
+        processor = new CommandProcessor(commands2);
+        processor.ProcessCommands();
+
+
+        // Keep the console window open until a key is pressed
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
